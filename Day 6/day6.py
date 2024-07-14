e12e0d5611e7a278386fbea135f09623878f5a3b
Jamiefirstday <jamie.lin8804@gmail.com>
2024-07-14 14:27:51 +0800

    Update day6.py

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,2 +1,18 @@
-# Using "Reeborg's world" and completed the Maze Challenge.
 # Learning python's function and while loop for Day 6.
+# Using "Reeborg's world" and completed the Maze Challenge.
+# Challenge link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+# Challenge code is below:
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+  
+# while not at_goal():
+#     if not wall_on_right():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         while not front_is_clear():
+#             turn_left()
